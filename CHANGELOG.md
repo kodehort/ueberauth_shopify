171da5fd9071286293552f8004ba6beabe665298
Alistair Stead <alistair_stead@me.com>
2016-11-04 16:08:30 +0000

    Release v0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# v 0.1.1
+
+* Update the dependencies to latest stable releases
+* Updated documentation to remove incorrect reference
+
 # v 0.1.0
 
 * Initial OS release
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule UeberauthShopify.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @url "https://github.com/alistairstead/ueberauth_shopify"
 
 
